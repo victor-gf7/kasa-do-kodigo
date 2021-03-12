6b71b990621de64e33b3a9ea50d67c7d7499f269
unknown <ferreirazaltan@gmail.com>
2021-03-12 15:12:42 -0300

    Validação de email único

--- src/main/kotlin/br/com/zup/validation/UniqueValueValidator.kt
+++ src/main/kotlin/br/com/zup/validation/UniqueValueValidator.kt
@@ -1,26 +1,26 @@
 package br.com.zup.validation
 
 
+import io.micronaut.core.annotation.AnnotationValue
+import io.micronaut.transaction.annotation.TransactionalAdvice
+import io.micronaut.validation.validator.constraints.ConstraintValidator
+import io.micronaut.validation.validator.constraints.ConstraintValidatorContext
+import javax.inject.Singleton
 import javax.persistence.EntityManager
-import javax.validation.ConstraintValidator
-import javax.validation.ConstraintValidatorContext
-import kotlin.reflect.KClass
 
+@Singleton
+@TransactionalAdvice
 class UniqueValueValidator(private val entityManager: EntityManager) : ConstraintValidator<UniqueValue, Any> {
 
-    lateinit var domainAttribute: String
-    lateinit var klass: KClass<*>
-
-    override fun initialize(constraintAnnotation: UniqueValue?) {
-        println("Initialize")
-        domainAttribute = constraintAnnotation!!.fieldName
-        klass = constraintAnnotation.domainClass
-    }
-
-    override fun isValid(value: Any?, context: ConstraintValidatorContext?): Boolean {
-        println("select 1 from ${klass.qualifiedName} where $domainAttribute = $value")
+    override fun isValid(
+        value: Any?,
+        annotationMetadata: AnnotationValue<UniqueValue>,
+        context: ConstraintValidatorContext
+    ): Boolean {
+        val domainAttribute = annotationMetadata.values.getValue("fieldName")
+        val klass = annotationMetadata.values.getValue("domainClass")
         return entityManager
-            .createQuery("select 1 from ${klass.qualifiedName} where $domainAttribute = $value")
+            .createQuery("select 1 from $klass where $domainAttribute = '$value'")
             .resultList
             .isEmpty()
     }
